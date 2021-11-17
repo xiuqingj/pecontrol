4c0457b31a1c3474fa0e4822b5774b0213741b0e
Sandra Jiang <sandra.jiang@puppet.com>
2021-11-17 16:30:26 +0800

    test message for agent-p

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,12 +25,12 @@ File { backup => false }
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
+node agent-p.puppetdebug.vlan {
   # This is where you can declare classes for all nodes.
   # Example:
-  #   class { 'my_class': }
-#  $msg = lookup('message')
-#  notify { $msg: }
+  # class { 'my_class': }
+   $msg = lookup('message')
+   notify { $msg: }
 }
 
 #node 'mom.platform9.puppet.net' {
