f7e2e7c3882e98f5343aa569e42aedce59235178
Sandra Jiang <sandra.jiang@puppet.com>
2021-11-17 16:35:34 +0800

    test out eyaml lookup

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,7 +25,7 @@ File { backup => false }
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node agent-p.puppetdebug.vlan {
+node 'agent-p.puppetdebug.vlan' {
   # This is where you can declare classes for all nodes.
   # Example:
   # class { 'my_class': }
